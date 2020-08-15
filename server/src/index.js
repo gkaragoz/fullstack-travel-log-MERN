7248fe1e88e063fd32ca5125c4e316bb97409808
gkaragoz <gkaragoz96@gmail.com>
2020-08-15 17:33:20 +0300

    Create logs.js and handle post and get requests for LogEntry model

--- server/src/index.js
+++ server/src/index.js
@@ -7,11 +7,13 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const middlewares = require("./middlewares");
+const logs = require("./api/logs");
 
 const app = express();
 
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 app.use(morgan("common"));
@@ -21,12 +23,15 @@ app.use(
     origin: process.env.CORS_ORIGIN,
   })
 );
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({
     message: "Hello World!",
   });
 });
+
+app.use("/api/logs", logs);
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
