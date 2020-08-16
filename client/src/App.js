1db46d77aed2f12350f1970beac4510c368167b3
gkaragoz <gkaragoz96@gmail.com>
2020-08-16 12:50:19 +0300

    Show map and fetch data from server. Add popup.

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
 
-function App() {
+import { listLogEntries } from "./API";
+
+const App = () => {
+  const [logEntries, setLogEntries] = useState([]);
+  const [showPopup, setShowPopup] = useState({});
+  const [viewport, setViewport] = useState({
+    width: "100vw",
+    height: "100vh",
+    latitude: 37.6,
+    longitude: -95.655,
+    zoom: 4,
+  });
+
+  useEffect(() => {
+    (async () => {
+      const logEntries = await listLogEntries();
+      setLogEntries(logEntries);
+    })();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ReactMapGL
+      {...viewport}
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+      mapStyle="mapbox://styles/annotto3/ckdwufagc0i6819o0us30ezsy"
+      onViewportChange={setViewport}
+    >
+      {logEntries.map((entry) => (
+        <>
+          <Marker
+            key={entry._id}
+            latitude={entry.latitude}
+            longitude={entry.longitude}
+          >
+            <div
+              onClick={() =>
+                setShowPopup({
+                  [entry._id]: true,
+                })
+              }
+            >
+              <img
+                className="marker"
+                style={{
+                  height: `${6 * viewport.zoom}px`,
+                  width: `${6 * viewport.zoom}px`,
+                }}
+                src={"./pin.svg"}
+                alt="marker"
+              />
+            </div>
+          </Marker>
+          {showPopup[entry._id] ? (
+            <Popup
+              latitude={entry.latitude}
+              longitude={entry.longitude}
+              closeButton={true}
+              closeOnClick={false}
+              dynamicPosition={true}
+              onClose={() => setShowPopup({})}
+              anchor="top"
+            >
+              <div className="popup">
+                <h3>{entry.title}</h3>
+                <p>{entry.comments}</p>
+                <small>
+                  Visited on: {new Date(entry.visitDate).toLocaleDateString()}
+                </small>
+              </div>
+            </Popup>
+          ) : null}
+        </>
+      ))}
+    </ReactMapGL>
   );
-}
+};
 
 export default App;
